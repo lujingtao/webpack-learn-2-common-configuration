d7bd075f1fc43ee5f95092a5992d6be19d147251
大话主席 <305491515@qq.com>
2019-09-24 19:09:36 +0800

    second push

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
-let ele = document.createElement('h1');
-ele.innerHTML = "Hello 大话主席11!";
+import "./index.scss";
+
+console.log("xx");
+var ele = document.createElement('h1');
+ele.innerHTML = "Hello 大话主席!";
+document.body.appendChild(ele);
+
+var ele = document.createElement('img');
+ele.src = "https://img.t.sinajs.cn/t6/style/images/global_nav/WB_logo.png";
 document.body.appendChild(ele);
--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,65 @@
+const MiniCssExtractPlugin = require('mini-css-extract-plugin'); //css文件抽离插件
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin'); //css压缩插件，生产模式有效
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin'); //js压缩插件，生产模式有效
+
 module.exports = {
-  entry:  __dirname + "/src/index.js",//唯一入口文件
+  mode: 'development',
+  devtool: 'source-map', //devtool模式有很多，自行百度
+  entry: __dirname + "/src/index.js",//唯一入口文件
   output: {
     path: __dirname + "/dist",//打包后的文件存放的地方
     filename: "bundle.js"//打包后输出文件的文件名
+  },
+  devServer: { //开发模式下的本地服务器
+    contentBase: './dist'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(css|scss)$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          {
+            loader: 'css-loader',
+            options: {
+              sourceMap: true
+            }
+          },
+          {
+            loader: 'sass-loader',
+            options: {
+              sourceMap: true
+            }
+          },
+          {
+            test: /\.(png|svg|jpg|gif)$/,
+            use: [
+              'file-loader'
+            ]
+          },
+          {
+            test: /\.(woff|woff2|eot|ttf|otf)$/,
+            use: [
+              'file-loader'
+            ]
+          }
+        ]
+      },
+    ]
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: 'index.css' // 打包到dist/index.css
+    })
+  ],
+  optimization: {
+    minimizer: [
+      new UglifyJsPlugin({
+        cache: true,
+        parallel: true,
+        sourceMap: true //是否需要sourceMap
+      }),
+      new OptimizeCSSAssetsPlugin({})
+    ]
   }
 }
